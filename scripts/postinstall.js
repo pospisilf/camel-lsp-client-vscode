fb5dee978f92ec981362b3668827118e4ea79a97
Filip Pospisil <fpospisi@redhat.com>
2024-10-13 21:32:19 +0200

    fix postintall

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,17 +2,17 @@
 
 const LSP_SERVER_VERSION = "1.26.0-SNAPSHOT";
 
-import download from "mvn-artifact-download";
+import { download } from "mvn-artifact-download"; // Named export
 import * as fs from 'fs';
 import * as path from 'path';
 
 const MAVEN_REPO_URL = 'https://oss.sonatype.org/content/repositories/snapshots/';
 
 download('com.github.camel-tooling:camel-lsp-server:' + LSP_SERVER_VERSION,
-	'./jars/', MAVEN_REPO_URL).then((filename)=>{
-	fs.renameSync(filename, path.join('.', 'jars', 'language-server.jar'));
-});
+	'./jars/', MAVEN_REPO_URL).then((filename) => {
+		fs.renameSync(filename, path.join('.', 'jars', 'language-server.jar'));
+	});
 
-download('com.github.camel-tooling:camel-lsp-server:json:cyclonedx:' + LSP_SERVER_VERSION, '.', MAVEN_REPO_URL).then((filename)=>{
+download('com.github.camel-tooling:camel-lsp-server:json:cyclonedx:' + LSP_SERVER_VERSION, '.', MAVEN_REPO_URL).then((filename) => {
 	fs.renameSync(filename, path.join('.', 'camel-ls-sbom.json'));
 });
